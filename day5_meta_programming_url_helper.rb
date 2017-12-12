3fe61fa025608368df159c4fd80e347bd012bc77
Thorsten Eckel <te@znuny.com>
2017-12-12 22:13:34 +0100

    Re-added unrefactored version of first metaprogramming example.

--- day5_meta_programming_url_helper.rb
+++ day5_meta_programming_url_helper.rb
@@ -1,55 +1,25 @@
 class URLHelper
 
-  attr_reader :model_class
-
   def initialize(model_class)
 
     @model_class = model_class
 
-    instance_methods.each do |method_name|
+    model_class.instance_methods(false).each do |method_name|
 
-      define_path_method(method_name) do
-        path_result(method_name)
+      # Would work too but is not so easy to read:
+      # path_method_name = [
+      #   method_name,
+      #   "#{model_class.name.downcase.gsub('::', '_')}s",
+      #   'path'
+      # ].join('_').to_sym
+      path_method_name = "#{method_name}_#{model_class.name.downcase.gsub('::', '_')}s_path".to_sym
+
+      self.class.send(:define_method, path_method_name) do
+        # Would work too but is not so easy to read:
+        # model_class.name.downcase.split('::').push(method_name).unshift(nil).join('/')
+        "/#{model_class.name.downcase.split('::').push(method_name).join('/')}"
       end
     end
-  end
-
-  private
-
-  def instance_methods
-    model_class.instance_methods(false)
-  end
-
-  def define_path_method(method_name, &block)
-    self.class.send(:define_method, path_method_name(method_name), &block)
-  end
-
-  def model_name
-    model_class.name
-  end
-
-  def model_name_downcased
-    model_name.downcase
-  end
-
-  def model_name_downcased_plurialized
-    "#{model_name_downcased}s"
-  end
-
-  def path_method_name(method_name)
-    # Would work too but is not so easy to read:
-    # [
-    #   method_name,
-    #   "#{model_name_downcased_plurialized.gsub('::', '_')}",
-    #   'path'
-    # ].join('_').to_sym
-    "#{method_name}_#{model_name_downcased_plurialized.gsub('::', '_')}_path".to_sym
-  end
-
-  def path_result(method_name)
-    # Would work too but is not so easy to read:
-    # model_name_downcased.split('::').push(method_name).unshift(nil).join('/')
-    "/#{model_name_downcased.split('::').push(method_name).join('/')}"
   end
 end
 
